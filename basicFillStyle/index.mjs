839f11525108ca24008ddb98408f8c74a1c6f5aa
ASCll <hzxbpld@163.com>
2020-08-22 23:57:35 +0800

    add: more example

--- basicFillStyle/index.mjs
+++ basicFillStyle/index.mjs
@@ -59,7 +59,46 @@
   );
 }
 
-
 {
-  
+  /** @type {HTMLCanvasElement} */
+  const canvasWithPattern = document.getElementById("canvasWithPattern");
+
+  const context = canvasWithPattern.getContext("2d");
+
+  const radios = {
+    repeat: document.getElementById("repeat"),
+    "repeat-x": document.getElementById("repeat-x"),
+    "repeat-y": document.getElementById("repeat-y"),
+    "no-repeat": document.getElementById("no-repeat"),
+  };
+
+  const image = new Image(60, 30);
+  image.width = 60;
+  image.height = 30;
+  image.src = "circle.png";
+
+  function fillCanvasWithPattern(repeatPatternString) {
+    context.clearRect(0, 0, canvasWithPattern.width, canvasWithPattern.height);
+    
+    const pattern = context.createPattern(image, repeatPatternString);
+
+    context.fillStyle = pattern;
+
+    context.fillRect(0, 0, canvasWithPattern.width, canvasWithPattern.height);
+  }
+
+  for (const [key, value] of Object.entries(radios)) {
+    value.addEventListener("click", () => fillCanvasWithPattern(key));
+    value.addEventListener("click", () => {
+      for (const [innerKey, innerValue] of Object.entries(radios)) {
+        if (innerKey !== key) {
+          innerValue.checked = false;
+        }
+      }
+    });
+  }
+
+  image.onload = function () {
+    fillCanvasWithPattern("repeat");
+  };
 }
---
+++ basicShadow/readme.md
@@ -0,0 +1,13 @@
+# 控制阴影
+
+主要有四个属性
+
+- shadowColor
+- shadowOffsetX
+- shadowOffsetY
+- shadowBlur
+
+绘制出的阴影可以用在:
+- 图形
+- 文本
+- 图形
